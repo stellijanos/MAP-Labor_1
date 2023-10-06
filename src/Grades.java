72b8260485b2c1df42af5957f5e7677036adb528
Janos Stelli <stellijanos23@gmail.com>
2023-10-06 20:37:05 +0300

    initiate round and create insuficient methods

---
+++ src/Grades.java
@@ -0,0 +1,10 @@
+public class Grades {
+    public int round(int grade) {
+
+        return 0;
+    }
+    public boolean insuficient(int grade) {
+        return grade< 38 || round(grade) < 40;
+    }
+}
+
