5715a4883c6a0983a4eee09646c7d556bb15bfe0
Janos Stelli <stellijanos23@gmail.com>
2023-10-06 22:04:22 +0300

    implement nextMultipleOf5 and round methods

--- src/Grades.java
+++ src/Grades.java
@@ -1,10 +1,34 @@
 public class Grades {
-    public int round(int grade) {
 
-        return 0;
+
+    /**
+     *
+     * @param number type of int
+     * @return next multiple of 5 after the given number
+     */
+    public int nextMultipleOf5(int number) {
+        return number / 5 * 5 + 5;
     }
+
+    /**
+     *
+     * @param grade type of int
+     * @return the grade rounded up if it ends with 3, 4, 8, 9,
+     */
+    public int round(int grade) {
+        return grade - nextMultipleOf5(grade) < 3 ? nextMultipleOf5(grade) : grade;
+    }
+
+    /**
+     *
+     * @param grade
+     * @return true if the grade is insuficient or false otherwise
+     */
     public boolean insuficient(int grade) {
-        return grade< 38 || round(grade) < 40;
+        return grade < 38 || round(grade) < 40;
     }
+
+
+
 }
 
