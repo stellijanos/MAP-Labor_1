9608d5c71cfa0416ff930df05baf820d8b17d05d
Janos Stelli <stellijanos23@gmail.com>
2023-10-06 22:46:04 +0300

    implement averageGrade and roundedGrades methods

--- src/Grades.java
+++ src/Grades.java
@@ -1,5 +1,6 @@
-public class Grades {
+import java.util.ArrayList;
 
+public class Grades {
 
     /**
      *
@@ -13,7 +14,7 @@ public class Grades {
     /**
      *
      * @param grade type of int
-     * @return the grade rounded up if it ends with 3, 4, 8, 9,
+     * @return rounded up grade if it ends with 3, 4, 8 or 9
      */
     public int round(int grade) {
         return grade - nextMultipleOf5(grade) < 3 ? nextMultipleOf5(grade) : grade;
@@ -21,14 +22,42 @@ public class Grades {
 
     /**
      *
-     * @param grade
+     * @param grade an Integer between 0 and 100
      * @return true if the grade is insuficient or false otherwise
      */
-    public boolean insuficient(int grade) {
+    public boolean isInsuficient(int grade) {
         return grade < 38 || round(grade) < 40;
     }
 
+    /**
+     *
+     * @param grades an array with Integer numbers between 0 and 100
+     * @return filtered grades by insuficiency
+     */
+    public ArrayList<Integer> insuficientGrades(ArrayList<Integer> grades) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (Integer grade: grades)
+            if (isInsuficient(grade))
+                result.add(grade);
+        return result;
+    }
+
+    public int sumOfArrayElements(ArrayList<Integer> array) {
+        int sum = 0;
+        for (Integer element: array) sum += element;
+        return sum;
+    }
+
+    public int averageGrade(ArrayList<Integer> grades) {
+        return sumOfArrayElements(grades) / grades.size();
+    }
 
 
+    public ArrayList<Integer> roundedGrades(ArrayList<Integer> grades) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        for (Integer grade: grades)
+            result.add(round(grade));
+        return result;
+    }
 }
 
