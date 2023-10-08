e009927c22c897d8ed8295335ef61a5348cf0994
Janos Stelli <stellijanos23@gmail.com>
2023-10-08 21:51:06 +0300

    implement test cases for sum without maximal number from Array class and simplify code

--- src/TestArray.java
+++ src/TestArray.java
@@ -3,14 +3,14 @@ public class TestArray {
 
         System.out.println("\n\nTests for Problem 2 - Array\n");
 
-        test_minimalNumber();
-        test_maximalNumber();
-        test_sumWithoutMinimalNumber();
-        test_sumWithoutMaximalNumber();
+        Array numbers = new Array(40, 55, 60, 3, 90, 87, 69, 43);
+        test_minimalNumber(numbers);
+        test_maximalNumber(numbers);
+        test_sumWithoutMinimalNumber(numbers);
+        test_sumWithoutMaximalNumber(numbers);
     }
 
-    private void test_minimalNumber() {
-        Array numbers = new Array(40, 55, 60, 3, 90, 87, 69, 43);
+    private void test_minimalNumber(Array numbers) {
         int result = numbers.minimalNumber();
         int expected = 3;
         int unexpected = 40;
@@ -30,8 +30,7 @@ public class TestArray {
         }
     }
 
-    private void test_maximalNumber() {
-        Array numbers = new Array(40, 55, 60, 3, 90, 87, 69, 43);
+    private void test_maximalNumber(Array numbers) {
         int result = numbers.maximalNumber();
         int expected = 90;
         int unexpected = 87;
@@ -51,8 +50,7 @@ public class TestArray {
         }
     }
 
-    private void test_sumWithoutMinimalNumber() {
-        Array numbers = new Array(40, 55, 60, 3, 90, 87, 69, 43);
+    private void test_sumWithoutMinimalNumber(Array numbers) {
         int result = numbers.sumWithoutMinimalNumber();
         int expected = 444;
         int unexpected = 447;
@@ -72,8 +70,24 @@ public class TestArray {
         }
     }
 
-    private void test_sumWithoutMaximalNumber() {
+    private void test_sumWithoutMaximalNumber(Array numbers) {
 
+        int result = numbers.sumWithoutMaximalNumber();
+        int expected = 357;
+        int unexpected = 447;
+
+        try {
+            assert result == expected;
+            System.out.println("Test success case - sum without minimal number => successful!");
+        } catch ( AssertionError e) {
+            System.out.println("Test success case - sum without minimal number => failed!");
+        }
+
+        try {
+            assert result == unexpected;
+            System.out.println("Test fail case - sum without minimal number => failed!");
+        } catch ( AssertionError e) {
+            System.out.println("Test fail case - sum without minimal number => successful!");
+        }
     }
-
 }
