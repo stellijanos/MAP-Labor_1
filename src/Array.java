d7ee2082ab627c4522e26c335ab8f6f2b9e675b3
Janos Stelli <stellijanos23@gmail.com>
2023-10-08 21:07:30 +0300

    implement maximalNumber method in Array class

--- src/Array.java
+++ src/Array.java
@@ -3,9 +3,18 @@ import java.util.ArrayList;
 public class Array {
     public int minimalNumber(ArrayList<Integer> array) {
         int minimalNumber = array.get(0);
-        for (Integer number : array)
+        for (Integer number: array)
             if (minimalNumber > number)
                 minimalNumber = number;
         return minimalNumber;
     }
+
+    public int maximalNumber(ArrayList<Integer> array) {
+        int maximalNumber = array.get(0);
+        for (Integer number: array)
+            if (maximalNumber < number)
+                maximalNumber = number;
+        return maximalNumber;
+    }
+
 }
