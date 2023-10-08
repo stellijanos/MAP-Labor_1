9aa60ca1f0aee083c165ee13ea7e7b71f176568e
Janos Stelli <stellijanos23@gmail.com>
2023-10-08 21:17:14 +0300

    initiate test class for Array class and also the test methods

---
+++ src/TestArray.java
@@ -0,0 +1,28 @@
+public class TestArray {
+    public void runTests() {
+
+        Array array = new Array();
+
+        test_minimalNumber(array);
+        test_maximalNumber(array);
+        test_sumWithoutMinimalNumber(array);
+        test_sumWithoutMaximalNumber(array);
+    }
+
+    private void test_minimalNumber(Array array) {
+
+    }
+
+    private void test_maximalNumber(Array array) {
+
+    }
+
+    private void test_sumWithoutMinimalNumber(Array array) {
+
+    }
+
+    private void test_sumWithoutMaximalNumber(Array array) {
+
+    }
+
+}
