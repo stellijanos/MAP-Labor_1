87ec3800f55b61813d7308705dc5e733debfec34
Janos Stelli <stellijanos23@gmail.com>
2023-10-08 11:43:26 +0300

    implement test cases for round method from Grades

--- src/TestGrades.java
+++ src/TestGrades.java
@@ -3,10 +3,10 @@ public class TestGrades {
     public void runTests() {
         Grades gradeHandler = new Grades();
         test_nextMultipleOf5(gradeHandler);
-//        test_round(gradeHandler);
+        test_round(gradeHandler);
     }
 
-    private boolean successCase(int[] numbers, int[] expected, Grades gradeHandler) {
+    private boolean successCase(int[] numbers, int[] expected, Grades gradeHandler, String methodName) {
         boolean success = true;
 
         for (int i = 0; i < numbers.length; i++) {
@@ -21,58 +21,64 @@ public class TestGrades {
 
     private boolean failCase(int[] numbers, int[] expected, Grades gradeHandler) {
         boolean fail = true;
-
         for (int i = 0; i< numbers.length; i++) {
             try {
                 assert gradeHandler.nextMultipleOf5(numbers[i]) == expected[i];
                 fail = false;
-            } catch (AssertionError e) {
-                //
-            }
+            } catch (AssertionError e) {/**/}
         }
         return fail;
     }
 
-
     private void test_nextMultipleOf5(Grades gradeHandler) {
-        int[] numbers = {0, 10, 21, 32, 43, 54, 65, 76, 87, 98, 100};
-        int[] expected = {5, 15, 25, 35, 45, 55, 70, 80, 90, 100, 105};
+        int[] numbers    = {0, 10, 21, 32, 43, 54, 65, 76, 87, 98, 100};
+        int[] expected   = {5, 15, 25, 35, 45, 55, 70, 80, 90, 100, 105};
         int[] unexpected = {0, 10, 20, 30, 40, 50, 65, 75, 85, 95, 100};
 
-        boolean expectedResult = successCase(numbers, expected, gradeHandler);
-        boolean unexpectedResult = failCase(numbers, unexpected, gradeHandler);
+        boolean expectedResult   = true;
+        boolean unexpectedResult = true;
 
-        System.out.println( "Test success case - multiple of 5 => " + (expectedResult ? "successful" : "failed") + '!');
+        for (int i = 0; i < numbers.length; i++) {
+            try {
+                assert gradeHandler.nextMultipleOf5(numbers[i]) == expected[i];
+            } catch (AssertionError e) {
+                expectedResult= false;
+            }
+
+            try {
+                assert gradeHandler.nextMultipleOf5(numbers[i]) == unexpected[i];
+                unexpectedResult = false;
+            } catch (AssertionError e) {/**/}
+        }
+
+        System.out.println("Test success case - multiple of 5 => " + (expectedResult ? "successful" : "failed") + '!');
         System.out.println("Test fail case - multiple of 5 => "  + (unexpectedResult ? "successful" : "failed") + '!');
     }
 
     public void test_round(Grades gradeHandler) {
 
-        int[] numbers = {0, 10, 21, 32, 43, 54, 65, 76, 87, 98, 100};
-        int[] expected = {0, 10, 21, 32, 45, 55, 65, 76, 87, 100, 100};
+        int[] numbers    = {0, 10, 21, 32, 43, 54, 65, 76, 87, 98, 100};
+        int[] expected   = {0, 10, 21, 32, 45, 55, 65, 76, 87, 100, 100};
         int[] unexpected = {5, 15, 20, 30, 40, 50, 70, 80, 90, 95, 105};
 
-        boolean failed = false;
+        boolean expectedResult   = true;
+        boolean unexpectedResult = true;
 
         for (int i = 0; i < numbers.length; i++) {
             try {
                 assert gradeHandler.round(numbers[i]) == expected[i];
             } catch (AssertionError e) {
-                failed = true;
+                expectedResult= false;
             }
-        }
-        System.out.println( "Test success case - round => " + (failed ? "failed" : "successful") + '!');
 
-        failed = false;
-
-        for (int i = 0; i< numbers.length; i++) {
             try {
-                assert gradeHandler.round(numbers[i]) != expected[i];
-            } catch (AssertionError e) {
-                failed = true;
-            }
+                assert gradeHandler.round(numbers[i]) == unexpected[i];
+                unexpectedResult = false;
+            } catch (AssertionError e) {/**/}
         }
-        System.out.println("Test fail case - round => "  + (failed ? "successful" : "failed") + '!');
+
+        System.out.println("Test success case - round => " + (expectedResult ? "successful" : "failed") + '!');
+        System.out.println("Test fail case - round => "  + (unexpectedResult ? "successful" : "failed") + '!');
     }
 
 }
