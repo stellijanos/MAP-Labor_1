2b1b8d997d374079c35c230019ae3978ab349870
Janos Stelli <stellijanos23@gmail.com>
2023-10-14 20:10:38 +0300

    implement most expensive affordable method for USB class

--- src/labor_1/classes/USB.java
+++ src/labor_1/classes/USB.java
@@ -1,4 +1,23 @@
 package labor_1.classes;
 
+import java.util.ArrayList;
+import java.util.Collections;
+
 public class USB extends PcAccessory {
+
+
+    private ArrayList<Integer> fiilterPrices(int buget) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int price : _priceList) {
+            if (price <= buget) {
+                result.add(price);
+            }
+        }
+        return result;
+    }
+
+    public int mostExpensiveAffordable(int buget) {
+        return Collections.max(fiilterPrices(buget));
+    }
 }
